8a8edebccc27f1de6e2cacf50687bca8f0e1a4e1
Ali Hodroj <aloshito2004@gmail.com>
2024-03-25 07:36:29 +0200

    -added sign up page

--- SkillSprint/Views/LoginView.swift
+++ SkillSprint/Views/LoginView.swift
@@ -11,116 +11,121 @@ struct LoginView: View {
     
     @State var email = ""
     @State var password = ""
+    @State var presentSignUp = false
     
     var body: some View {
-        VStack(spacing: 40) {
-            
-            // MARK: heading image
-            Image("People_Thinking")
-                .resizable()
-                .frame(maxWidth: .infinity)
-                .frame(height: 280)
-                .padding(.horizontal, 40)
-            
-            // MARK: heading text
-            VStack(alignment: .leading, spacing: 5) {
-                Text("Login")
-                    .font(.largeTitle.bold())
-                    .foregroundStyle(.black)
-                Text("Enter your email & password")
-                    .font(.subheadline)
-                    .foregroundStyle(.black)
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.horizontal, 25)
-            
-            // MARK: email & password fields
-            VStack(spacing: 20) {
-                // email
-                HStack(spacing: 15) {
-                    Image(systemName: "at")
-                        .foregroundStyle(.gray)
-                        .font(.title2)
-                    TextField("", text: $email)
-                        .frame(maxWidth: .infinity)
-                        .frame(height: 35)
-                        .background {
-                            VStack(alignment: .leading, spacing: 5) {
-                                Text("Email")
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .foregroundStyle(.gray)
-                                Color.gray
-                                    .frame(height: 1)
-                            }
-                        }
-                }
-                // password
-                HStack(spacing: 15) {
-                    Image(systemName: "lock")
-                        .foregroundStyle(.gray)
-                        .font(.title2)
-                    TextField("", text: $password)
-                        .frame(maxWidth: .infinity)
-                        .frame(height: 35)
-                        .background {
-                            VStack(alignment: .leading, spacing: 5) {
-                                Text("Password")
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .foregroundStyle(.gray)
-                                Color.gray
-                                    .frame(height: 1)
-                            }
-                        }
-                }
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.horizontal, 25)
-            
-            Spacer()
-            
-            // MARK: login and signup buttons
-            VStack(spacing: 5) {
-                Button {
-                    
-                } label: {
+        NavigationStack {
+            VStack(spacing: 40) {
+                
+                // MARK: heading image
+                Image("people_thinking")
+                    .resizable()
+                    .frame(maxWidth: .infinity)
+                    .frame(height: UIScreen.main.bounds.width - 100)
+                    .padding(.horizontal, 40)
+                
+                // MARK: heading text
+                VStack(alignment: .leading, spacing: 5) {
                     Text("Login")
-                        .bold()
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .frame(height: 40)
-                        .foregroundStyle(.white)
-                        .background { Color.accentColor.clipShape(RoundedRectangle(cornerRadius: 12)) }
+                        .font(.largeTitle.bold())
+                        .foregroundStyle(.black)
+                    Text("Enter your email & password")
+                        .font(.subheadline)
+                        .foregroundStyle(.black)
                 }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 25)
                 
-                HStack {
-                    Color.black.clipShape(RoundedRectangle(cornerRadius: 22))
-                        .frame(height: 1)
-                    Text("OR")
-                        .font(.caption)
-                        .fontWeight(.light)
-                    Color.black.clipShape(RoundedRectangle(cornerRadius: 22))
-                        .frame(height: 1)
+                // MARK: email & password fields
+                VStack(spacing: 20) {
+                    // email
+                    HStack(spacing: 15) {
+                        Image(systemName: "at")
+                            .foregroundStyle(.gray)
+                            .font(.title2)
+                        TextField("", text: $email)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 35)
+                            .background {
+                                VStack(alignment: .leading, spacing: 5) {
+                                    Text("Email")
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                        .foregroundStyle(.gray)
+                                    Color.gray
+                                        .frame(height: 1)
+                                }
+                            }
+                    }
+                    // password
+                    HStack(spacing: 15) {
+                        Image(systemName: "lock")
+                            .foregroundStyle(.gray)
+                            .font(.title2)
+                        TextField("", text: $password)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 35)
+                            .background {
+                                VStack(alignment: .leading, spacing: 5) {
+                                    Text("Password")
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                        .foregroundStyle(.gray)
+                                    Color.gray
+                                        .frame(height: 1)
+                                }
+                            }
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 25)
+                
+                Spacer()
+                
+                // MARK: login and signup buttons
+                VStack(spacing: 5) {
+                    Button {
+                        
+                    } label: {
+                        Text("Login")
+                            .bold()
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .frame(height: 40)
+                            .foregroundStyle(.white)
+                            .background { Color.accentColor.clipShape(RoundedRectangle(cornerRadius: 12)) }
+                    }
+                    
+                    HStack {
+                        Color.black.clipShape(RoundedRectangle(cornerRadius: 22))
+                            .frame(height: 1)
+                        Text("OR")
+                            .font(.caption)
+                            .fontWeight(.light)
+                        Color.black.clipShape(RoundedRectangle(cornerRadius: 22))
+                            .frame(height: 1)
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding(.horizontal, 20)
+                    
+                    Button {
+                        self.presentSignUp = true
+                    } label: {
+                        Text("Sign Up")
+                            .bold()
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .frame(height: 40)
+                            .foregroundStyle(.white)
+                            .background { Color.accentColor.clipShape(RoundedRectangle(cornerRadius: 12)) }
+                    }
+                    
                 }
                 .frame(maxWidth: .infinity)
-                .padding(.horizontal, 20)
-                
-                Button {
-                    // todo
-                } label: {
-                    Text("Sign Up")
-                        .bold()
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .frame(height: 40)
-                        .foregroundStyle(.white)
-                        .background { Color.accentColor.clipShape(RoundedRectangle(cornerRadius: 12)) }
-                }
+                .padding(.horizontal, 25)
                 
             }
-            .frame(maxWidth: .infinity)
-            .padding(.horizontal, 25)
-            
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+            .background { Color.white.ignoresSafeArea() }
+            .padding(.vertical)
+            .navigationDestination(isPresented: $presentSignUp) { SignUpView() }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-        .padding(.vertical)
     }
 }
 
