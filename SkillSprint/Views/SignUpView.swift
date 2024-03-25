e6ef97297e501df8e39760ffc2efe784d175925b
Ali Hodroj <aloshito2004@gmail.com>
2024-03-25 07:34:26 +0200

    -added sign up view

---
+++ SkillSprint/Views/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  SkillSprint
+//
+//  Created by Ali Hodroj on 25/03/2024.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
